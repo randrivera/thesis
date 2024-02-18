fcab5512ef089c8b9249b3dd874b4e8b79aa25d6
randrivera <rand.k.rivera@gmail.com>
2024-02-18 12:17:16 -0500

    i dont even know

--- src/js/save.js
+++ src/js/save.js
@@ -1,18 +1,59 @@
-let gallery = document.querySelector("gallery");
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+ import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ import {
+  player
+} from "./script.js";
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: "AIzaSyAaomt0YCnQ2b67QihmeI9-JlQuXzEVU7I",
+   authDomain: "ai-faces.firebaseapp.com",
+   projectId: "ai-faces",
+   storageBucket: "ai-faces.appspot.com",
+   messagingSenderId: "338300608687",
+   appId: "1:338300608687:web:2f3209570f4bb0d91d745a",
+   measurementId: "G-7FBSYZXNKT"
+ };
+ 
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ const analytics = getAnalytics(app);
+ const db = getDatabase();
+ const reference = ref(db, 'scores');
+
+ function writeUserData(name, imageURL){
+  push(reference, {
+    username: name,
+    profile_picture: imageURL
+  }, { merge: true })
+
+  updateGallery()
+}
+
+writeUserData(player.name, player.imageURL);
+console.log("completed save.js")
 
 
+function updateGallery(){
+  console.log("Update Gallery succesful")
+}
 
-let imgArray = [];
+// function gotData(data){
+//   console.log(data);
+// }
 
-for(let i=1; i<6; i++){
-	imgArray[i]= `./assets/savedFranks/${i}.png`
-};
+// function errData(err){
+//   console.log("Error!");
+//   console.log(err)
 
+// }
 
-document.getElementById('saveToImage').addEventListener('click', function() {
-	let canvas = document.querySelector("canvas");
-    let dataURL = canvas.toDataURL("image/png", 1.0);
-    imgArray[imgArray.length]=`${dataURL}`
-    console.log('worked');
-}, false);
-
+ 
